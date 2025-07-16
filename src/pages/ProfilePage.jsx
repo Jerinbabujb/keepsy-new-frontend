a5dbfc14bc01a6d7be9a168341caa3ab25f6daf4
JERIN BABU <114816204+Jerinbabujb@users.noreply.github.com>
2025-07-16 18:32:59 +0300

    commit 12
    changed onSubmit to onClick in profile page due to redirect issue

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -31,7 +31,7 @@ const ProfilePage = () => {
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
-        <form className='flex flex-col gap-5 p-10 flex-1' onSubmit={handleSubmit}> 
+        <form className='flex flex-col gap-5 p-10 flex-1'> 
           <h3 className='text-lg '>Profile details</h3>
           <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer'>
           <input onChange={(e)=>setSelectedImg(e.target.files[0])} type='file' id='avatar' accept='.png, .jpeg, .jpg' hidden/>
@@ -40,7 +40,7 @@ const ProfilePage = () => {
           </label>
           <input type='text'onChange={(e)=>setName(e.target.value)} value={name} placeholder='Your name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' requied/>
           <textarea placeholder='write profile bio' onChange={(e)=>setBio(e.target.value)} value={bio} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4} required/>
-          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'> save</button>
+          <button onClick={handleSubmit} type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'> save</button>
            </form>
           <img src={authUser?.profilePic ||'/assets/keepsy'} alt='' className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`}/>
        
